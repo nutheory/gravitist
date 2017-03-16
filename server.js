1a62531ddf07ef72d8e17e0179084d74b86c1a42
Derek Rush <drush81@gmail.com>
2017-03-16 15:19:17 -0700

    server

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 
+var express = require('express'),
+    app = express()
 
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/dist'))
 
+app.get('/', function(request, response) {
+})
 
-
-// app.use(hpp())
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'))
+})
