1b4bc037b123e9c43663e40c37f35015c39fbcc1
Derek Rush <drush81@gmail.com>
2017-04-22 16:50:53 -0700

    new look

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ if (app.get('is_dev')) {
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve() + "/dist/index")
+  res.sendFile(path.resolve() + "/dist/index.html")
 })
 
 module.exports = app
