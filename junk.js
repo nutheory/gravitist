1f2e49c59a92219c6498df2aa33547818c7330a7
Derek Rush <drush81@gmail.com>
2017-07-01 01:43:39 -0700

    staring admin areas after graphql

--- junk.js
+++ junk.js
@@ -3,3 +3,11 @@
 "start-local": "nodemon index.js --ignore client --exec babel-node",
 "start-dev": "node backend-dev.js",
 "build-dev": "webpack --config webpack.config.js --progress",
+
+
+"start": "npm-run-all --parallel watch:server watch:build",
+"watch:build": "webpack --config webpack.config.js --watch",
+"watch:server": "nodemon \"./index.babel.js\" --watch \"./server\"",
+"start-dev": "node backend-dev.js",
+"build-dev": "webpack --config webpack.config.js --progress",
+"test": "test"
